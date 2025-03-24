9f7993a87bd29b1397fb9ad7e361d2dcc784064f
丁宇城 <23371414@buaa.edu.cn>
2025-03-24 10:43:42 +0800

    queue

--- include/queue.h
+++ include/queue.h
@@ -114,6 +114,12 @@
  */
 #define LIST_INSERT_AFTER(listelm, elm, field)                                                     \
 	/* Exercise 2.2: Your code here. */  \
+	do {                                                                                  \
+		if ((LIST_NEXT((elm), field) = LIST_NEXT((listelm), field)) != NULL)              \
+			LIST_NEXT((listelm), field)->field.le_prev = &LIST_NEXT((elm), field);        \
+		LIST_NEXT((listelm), field) = (elm);                                              \
+		(elm)->field.le_prev = &LIST_NEXT((listelm), field);                              \
+	} while (0)
 
 /*
  * Insert the element "elm" *before* the element "listelm" which is
